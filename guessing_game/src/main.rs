8abb80404cd1e99bc0bbb06e6b464048219f9d2c
ahnanne <54733637+ahnanne@users.noreply.github.com>
2022-02-05 15:15:07 +0900

    rand 크레이트로 난수 만들기

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,12 @@
+use rand::Rng;
 use std::io;
 
 fn main() {
     println!("숫자를 맞혀봅시다!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    println!("사용자가 맞혀야 할 숫자: {}", secret_number); // TODO: 테스트 후 제거할 코드
 
     println!("정답이라고 생각하는 숫자를 입력하세요.");
 

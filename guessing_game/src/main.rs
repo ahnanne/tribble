1a982ca27c54e2773dac8e3c49357a0ba9d18cc4
ahnanne <54733637+ahnanne@users.noreply.github.com>
2022-02-19 23:27:39 +0900

    loop, break

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -9,24 +9,30 @@ fn main() {
 
     println!("사용자가 맞혀야 할 숫자: {}", secret_number); // TODO: 테스트 후 제거할 코드
 
-    println!("정답이라고 생각하는 숫자를 입력하세요.");
-
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("입력한 값을 읽지 못했습니다.");
-
-    let guess: u32 = guess
-        .trim()
-        .parse()
-        .expect("입력한 값이 올바른 숫자가 아닙니다.");
-
-    println!("입력한 값: {}", guess);
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("입력한 숫자가 작습니다!"),
-        Ordering::Greater => println!("입력한 숫자가 큽니다!"),
-        Ordering::Equal => println!("정답!"),
+    loop {
+        println!("정답이라고 생각하는 숫자를 입력하세요.");
+    
+        let mut guess = String::new();
+    
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("입력한 값을 읽지 못했습니다.");
+    
+        let guess: u32 = guess
+            .trim()
+            .parse()
+            .expect("입력한 값이 올바른 숫자가 아닙니다.");
+    
+        println!("입력한 값: {}", guess);
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("입력한 숫자가 작습니다!"),
+            Ordering::Greater => println!("입력한 숫자가 큽니다!"),
+            Ordering::Equal => {
+                println!("정답!");
+                break;
+            },
+        }
     }
+
 }

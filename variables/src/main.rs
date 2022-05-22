167aea470d31bf145366fba442a1ddf9ec7d9a37
안예인 <54733637+ahnanne@users.noreply.github.com>
2022-05-23 00:21:59 +0900

    변수 가리기

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -5,9 +5,21 @@ fn main() {
     x = 6;
     println!("변경된 x의 값: {}", x);
 
+    // x = "hi";
+    // mismatched types - expected integer, found `&str` - rustc(E0308)
+
     let y = 7;
     println!("y의 값: {}", y);
 
     // y = 8;
     // cannot assign twice to immutable variable - rustc(E0384)
+
+    // 변수 가리기
+    let a = 1;
+    let a = a * 2;
+    let a = a * 10;
+    println!("a의 값은 ... {}", a); // a의 값은 ... 20
+
+    let a = "hi";
+    println!("a의 값은 ... {}", a); // a의 값은 ... hi
 }

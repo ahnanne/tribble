3e392df8382350f8e7a5bbfb2097f4a654c29f02
ahnanne <54733637+ahnanne@users.noreply.github.com>
2022-06-15 00:06:12 +0900

    구문(statement)과 표현식(expression)의 차이
    - 러스트는 표현식 기반 언어다.

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,9 +1,16 @@
 fn main() {
-    println!("오버워치2 출시일은!?");
+    println!("숫자 더하기");
 
     get_date(2022, 10, 5);
 }
 
 fn get_date(year: i32, month: i32, day: i32) {
-    println!("{}년 {}월 {}일", year, month, day);
+    let a = 1000;
+
+    let b = {
+        let sum = year + month + day; // 구문 -> 값을 반환하지 않는다!
+        sum - 2000 // 표현식 -> 세미콜론을 포함하지 않는다!
+    };
+
+    println!("a 더하기 b는...: {}", a + b);
 }

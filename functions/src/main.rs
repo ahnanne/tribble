a4df031453887cff04d49dcbd22709a250e2fea5
ahnanne <54733637+ahnanne@users.noreply.github.com>
2022-06-14 23:50:50 +0900

    매개변수

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
     println!("오버워치2 출시일은!?");
 
-    get_date();
+    get_date(2022, 10, 5);
 }
 
-fn get_date() {
-    println!("2022년 10월 5일");
+fn get_date(year: i32, month: i32, day: i32) {
+    println!("{}년 {}월 {}일", year, month, day);
 }

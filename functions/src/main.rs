277bbc50b9226ea9e7dbbbc64bec72d1dc138f3b
ahnanne <54733637+ahnanne@users.noreply.github.com>
2022-06-15 23:32:37 +0900

    값을 반환하는 함수

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -13,4 +13,20 @@ fn get_date(year: i32, month: i32, day: i32) {
     };
 
     println!("a 더하기 b는...: {}", a + b);
+
+    let x = first_five();
+    let y = second_five();
+
+    println!("{} + {} = {}", x, y, x + y);
 }
+
+fn first_five() -> i32 {
+    // return 타입은 이와 같이 꼭 적어줘야 한다.
+    return 5; // 이렇게 return 키워드와 함께 값을 반환해도 되지만,
+}
+
+fn second_five() -> i32 {
+    5 // 대부분의 함수는 이렇게 마지막 표현식의 결과를 반환한다.
+}
+
+// 어떤 함수가 반환하려는 값이 없을 경우에는 빈 튜플인 ()로 표현된다.
